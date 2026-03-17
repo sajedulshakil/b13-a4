a96870ec534c6bfc4b03ca7c4b723610f95968ce
Sajedul Alam Shakil <sajedulmds5@gmail.com>
2026-03-17 11:41:55 +0600

    stats count,delete button ok and functionalize no data available showing

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const interviewContainer = document.getElementById('interview-container')
 const rejectContainer = document.getElementById('reject-container')
 
 
-
+const emptyState = document.getElementById('empty-state')
 
 
 
@@ -40,18 +40,88 @@ for(const section of pages){
     section.classList.add('hidden')
 }
 
+emptyState.classList.add('hidden')
+
 
     if(tab === 'all'){
         allContainer.classList.remove('hidden')
+        if(allContainer.children.length < 1){
+            emptyState.classList.remove('hidden')
+        }
     }
 
     else if(tab === 'interview'){
         interviewContainer.classList.remove('hidden')
+        if(interviewContainer.children.length < 1){
+            emptyState.classList.remove('hidden')
+        }
     }
 
     else{
         rejectContainer.classList.remove('hidden')
+          if(rejectContainer.children.length < 1){
+            emptyState.classList.remove('hidden')
+        }
     }
 }
 
+
+// Stat update
+
+const totalStat = document.getElementById('stat-total')
+const interviewStat = document.getElementById('stat-interview')
+const rejectStat = document.getElementById('stat-reject')
+
+totalStat.innerText = allContainer.children.length
+
 switchTab(currentTab)
+
+const jobsContainer = document.getElementById('jobs-container').addEventListener('click', function(event){
+   
+    const clickedElement = event.target
+
+    const card = clickedElement.closest(".card")
+    // console.log(card)
+    const status = card.querySelector(".status")
+
+    const parent = card.parentNode
+
+    if(clickedElement.classList.contains("interview")){
+
+        status.innerText = "Interviewed"
+     interviewContainer.appendChild(card)
+     updateStat ()
+    }
+    if(clickedElement.classList.contains("reject")){
+
+    status.innerText = "Rejected"
+    rejectContainer.appendChild(card)
+    updateStat ()
+    }
+    if(clickedElement.classList.contains("delete")){
+     
+    parent.removeChild(card)
+    updateStat ()
+        
+    }
+})
+
+function updateStat (){
+    // totalStat.innerText = allContainer.children.length
+    // interviewStat.innerText = interviewContainer.children.length
+    // rejectStat.innerText = rejectContainer.children.length
+
+
+    const counts = {
+        all: allContainer.children.length,
+        interview: interviewContainer.children.length,
+        rejected: rejectContainer.children.length,
+    }
+
+     totalStat.innerText = counts["all"]
+     interviewStat.innerText = counts["interview"]
+     rejectStat.innerText = counts["rejected"]
+
+}
+
+updateStat ()
